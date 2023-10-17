3443217176f077bad461dd4508bf6664004c038f
artyom <artyom27000@mail.ru>
2023-10-17 23:59:21 +0300

    trash delete

--- keybroadTel/keybroad.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-keyboard_client = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='/Запуск Коня'),KeyboardButton(text='/список настроек')],
-    ],
-    resize_keyboard=True
-)
-
