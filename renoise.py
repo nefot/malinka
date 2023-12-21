16640674a5971aea0380aa10f1b202dafc2687a6
artyom <artyom27000@mail.ru>
2023-12-22 01:20:34 +0300

    renoise.py_ test 2

--- renoise.py
+++ renoise.py
@@ -2,6 +2,14 @@ from rnnoise_wrapper import RNNoise
 
 denoiser = RNNoise(f_name_lib='librnnoise_default.so.0.4.1')
 
-audio = denoiser.read_wav('test.wav')
-denoised_audio = denoiser.filter(audio)
-denoiser.write_wav('test_denoised.wav', denoised_audio)
+
+def reNoise(audio_data: object) -> object:
+    return denoiser.filter(audio_data)
+
+
+if __name__ == '__main__':
+    denoiser = RNNoise(f_name_lib='librnnoise_default.so.0.4.1')
+
+    audio = denoiser.read_wav('test.wav')
+    denoised_audio = denoiser.filter(audio)
+    denoiser.write_wav('test_denoised.wav', denoised_audio)
